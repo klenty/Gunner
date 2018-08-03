a7a7eedf4fbda0a1a47ec966fbc92cf2351999bf
Muthu Kumar <muthukumar@thefeathers.in>
2018-08-03 13:43:35 +0530

    [run] Support for multiple expect per test item

--- gunner/lib/runTests.js
+++ gunner/lib/runTests.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const { pass, fail } = require('../constants');
+const { pass, fail } = require('./constants');
 
-const runTests = tests => Promise.all(tests.map(test =>
-	test.test()
-	.then(() => ({ description: test.description, result: pass }))
-	.catch(e => ({ description: test.description, result: fail, error: e }))
-));
+const runTests = tests => Promise.all(tests.map(each => {
+	const pred = each.test();
+	return (Array.isArray(pred) ? Promise.all(pred) : pred)
+	.then(() => ({ description: each.description, result: pass }))
+	.catch(e => ({ description: each.description, result: fail, error: e }));
+}));
 
 module.exports = runTests;
